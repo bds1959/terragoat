1036fc6e49578f98247031d7bb21f185edc372cd
murali <murali@banyandata.com>
2021-11-29 11:05:59 +0530

    Update vars.tf

--- terraform/simple_instance/vars.tf
+++ terraform/simple_instance/vars.tf
@@ -1,7 +1,7 @@
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
 variable "region" {
-        default = "us-west-2"
+        default = "ap-south-1"
+}
+
+variable "profile" {
+    description = "AWS credentials profile you want to use"
 }
